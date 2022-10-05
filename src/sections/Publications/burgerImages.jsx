2cdd90dbf01ef4f364269676c6331e31e56917b7
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-10-05 23:10:45 +0100

    Publications section update

---
+++ src/sections/Publications/burgerImages.jsx
@@ -0,0 +1,12 @@
+import Burger1 from '../../assets/img/Burger1.svg'
+import Burger2 from '../../assets/img/Burger2.svg'
+import Burger4 from '../../assets/img/Burger4.svg'
+import Burger5 from '../../assets/img/Burger5.svg'
+import Burger6 from '../../assets/img/Burger6.svg'
+import Burger7 from '../../assets/img/Burger1.svg'
+import Burger8 from '../../assets/img/Burger2.svg'
+import Burger10 from '../../assets/img/Burger4.svg'
+import Burger11 from '../../assets/img/Burger5.svg'
+import Burger12 from '../../assets/img/Burger6.svg'
+
+export default [Burger1, Burger2, Burger4, Burger5, Burger6, Burger7, Burger8, Burger10, Burger11, Burger12]
--- src/sections/Publications/index.jsx
+++ src/sections/Publications/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
+import burgerImages from './burgerImages'
+
 function Publications() {
     return (
         <div className={styles.container}>
-            PUBLICATIONS
+            <div className={styles.content}>
+                {burgerImages.map((image) => {
+                    return (
+                        <img src={image} alt="" />
+                    )
+                })}
+            </div>
         </div>
     )
 }
