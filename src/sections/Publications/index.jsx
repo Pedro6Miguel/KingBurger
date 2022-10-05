9f7ca6f2823bef975caed1cf1ab5f407e8829d3e
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-10-05 23:27:51 +0100

    Update

--- src/sections/Publications/index.jsx
+++ src/sections/Publications/index.jsx
@@ -6,12 +6,19 @@ import burgerImages from './burgerImages'
 function Publications() {
     return (
         <div className={styles.container}>
+            <div className={styles.instagram}>
+                <h3>Publicações do instagram</h3>
+                <p>Todos os nossos clientes são tratados como rei e rainha, com a nossa colunaria artesanal.</p>
+            </div>
             <div className={styles.content}>
                 {burgerImages.map((image) => {
                     return (
                         <img src={image} alt="" />
                     )
                 })}
+            </div>
+            <div className={styles.hashtag}>
+                <h3>#empireburger • #empireburger • #empireburger • #empireburger • #empireburger • #empireburger • #empireburger • #empireburger • #empireburger</h3>
             </div>
         </div>
     )
