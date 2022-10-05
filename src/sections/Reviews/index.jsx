29207d9837b91230f5207d4d4b337f4388bd119e
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-10-05 22:18:18 +0100

    Reviews section update

--- src/sections/Reviews/index.jsx
+++ src/sections/Reviews/index.jsx
@@ -1,10 +1,94 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
+import { motion } from "framer-motion"
+import { useRef, useEffect, useState } from "react"
+
+import ProfilePicture from '../../assets/img/ProfilePicture.svg'
+
 function Reviews() {
+
+    const [width, setWidth] = useState(0);
+    const carousel = useRef();
+
+    useEffect(() => {
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
+    }, []);
+
     return (
-        <div className={styles.container}>
-            REVIEWS
+        <div className={styles.body}>
+            <h3>Nossa realeza</h3>
+            <p className={styles.body_text}>A satisfação de nossos clientes em primeiro lugar!</p>
+
+            <motion.div ref={carousel} whileTap={{ cursor: "grabbing" }} className={styles.carousel}>
+                <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className={styles.inner_carousel}>
+                    <div className={styles.item}>
+                        <p className={styles.review}>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin.”</p>
+                        <div className={styles.person_info}>
+                            <img src={ProfilePicture} alt="" />
+                            <div>
+                                <h5>Carla Gomes</h5>
+                                <p>23 Anos • Designer</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.item}>
+                        <p className={styles.review}>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin.”</p>
+                        <div className={styles.person_info}>
+                            <img src={ProfilePicture} alt="" />
+                            <div>
+                                <h5>Carla Gomes</h5>
+                                <p>23 Anos • Designer</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.item}>
+                        <p className={styles.review}>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin.”</p>
+                        <div className={styles.person_info}>
+                            <img src={ProfilePicture} alt="" />
+                            <div>
+                                <h5>Carla Gomes</h5>
+                                <p>23 Anos • Designer</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.item}>
+                        <p className={styles.review}>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin.”</p>
+                        <div className={styles.person_info}>
+                            <img src={ProfilePicture} alt="" />
+                            <div>
+                                <h5>Carla Gomes</h5>
+                                <p>23 Anos • Designer</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.item}>
+                        <p className={styles.review}>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin.”</p>
+                        <div className={styles.person_info}>
+                            <img src={ProfilePicture} alt="" />
+                            <div>
+                                <h5>Carla Gomes</h5>
+                                <p>23 Anos • Designer</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.item}>
+                        <p className={styles.review}>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin.”</p>
+                        <div className={styles.person_info}>
+                            <img src={ProfilePicture} alt="" />
+                            <div>
+                                <h5>Carla Gomes</h5>
+                                <p>23 Anos • Designer</p>
+                            </div>
+                        </div>
+                    </div>
+                </motion.div>
+            </motion.div>
         </div>
     )
 }
