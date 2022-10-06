409ad5698142a46055fac3b6737e895f4fc5ffeb
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-10-06 18:04:26 +0100

    Update

--- src/sections/Publications/burgerImages.jsx
+++ src/sections/Publications/burgerImages.jsx
@@ -1,12 +1,10 @@
-import Burger1 from '../../assets/img/Burger1.svg'
-import Burger2 from '../../assets/img/Burger2.svg'
-import Burger4 from '../../assets/img/Burger4.svg'
-import Burger5 from '../../assets/img/Burger5.svg'
-import Burger6 from '../../assets/img/Burger6.svg'
-import Burger7 from '../../assets/img/Burger1.svg'
-import Burger8 from '../../assets/img/Burger2.svg'
-import Burger10 from '../../assets/img/Burger4.svg'
-import Burger11 from '../../assets/img/Burger5.svg'
-import Burger12 from '../../assets/img/Burger6.svg'
+import Burger1 from '../../assets/img/Burger1.png'
+import Burger2 from '../../assets/img/Burger2.png'
+import Burger3 from '../../assets/img/Burger3.png'
+import Burger4 from '../../assets/img/Burger4.png'
+import Burger5 from '../../assets/img/Burger5.png'
+import Burger6 from '../../assets/img/Burger6.png'
+import Burger7 from '../../assets/img/Burger1.png'
+import Burger8 from '../../assets/img/Burger2.png'
 
-export default [Burger1, Burger2, Burger4, Burger5, Burger6, Burger7, Burger8, Burger10, Burger11, Burger12]
+export default [Burger1, Burger2, Burger3, Burger4, Burger5, Burger6, Burger7, Burger8]
